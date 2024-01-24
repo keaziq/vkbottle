927debfc9c9bcf894efbe78e5f5509490f432277
Roman <40473552+keaziq@users.noreply.github.com>
2024-01-24 21:02:36 +0300

    Update rom.py

--- python/rom.py
+++ python/rom.py
@@ -5,7 +5,7 @@ from pyowm.owm import OWM
 
 import os
 
-bot = Bot(token="")
+bot = Bot(token="vk1.a.hHMpk86LD-qDpLrscN9vz-NV5gYnf5QIU3Cz5-tJAdZdqKP92B2c_XfAtRF5M3rcyUVtr3dl2OG0MPLEwKD0tbj2fcVYF56HCcQx7ZbMBXc8yIWhJSPb_BrJutS6wk0iWRr1_PJalVlp2QveywAdugnYmc6cVZrcfyaPtCWecyoGLGEToTioN-J8Nxc8q4Duy8mlUK--NtZxH24R7v3xxA")
 keyboard = Keyboard(one_time=True, inline=False)
 
 hello = ["привет",'start','хай',]
