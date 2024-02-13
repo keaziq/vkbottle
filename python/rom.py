b8f40c70ad3610b7d5c289d455b0d77a493703a0
Roman <40473552+keaziq@users.noreply.github.com>
2024-02-13 14:10:15 +0300

    Update rom.py

--- python/rom.py
+++ python/rom.py
@@ -1,35 +1,41 @@
-import json
+
 from vkbottle.bot import Bot, Message
 from vkbottle import Keyboard, KeyboardButtonColor, Text, PhotoMessageUploader, Location, OpenLink, BaseStateGroup, CtxStorage
 from vkbottle.tools.dev.mini_types.bot.message import message_min
 from config import *
+import json
+import random
 import requests
+from typing import Optional
 import datetime
 from pyowm import OWM
-from translate import Translator
+import goslate
+from random import randint
 import asyncio
-
+from os.path import abspath as tdir
 ctx_storage = CtxStorage()
 bot = Bot(token=token)
 keyboard = Keyboard(one_time=True, inline=False)
 photo_uploader = PhotoMessageUploader(bot.api)
-hello = ["привет",'start','хай',]
-ru_letters = "aбвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-en_letters = "abcdefghijklmnopqrstuvwxyz"
+hello = ["привет",'start','хай','Ку-ку','здарова','здраствуйте','здраствуй']
+gs = goslate.Goslate()
 
+number_to_guess= random.randint(0,10)
+is_game_started = False
 
+class Number(BaseStateGroup):
 
+  numbers = None
 
 
 class Translate(BaseStateGroup):
 
   translator = None
+  language = None
 
 class Weather(BaseStateGroup):
 
     city = None
-
-
 
 
 @bot.on.message(text=hello)
@@ -47,7 +53,7 @@ async def menu(message: Message):
     keyboard = Keyboard(one_time=True)
 
     keyboard.add(Text("Что ты умеешь?", {"cmd": "я умею"}), color=KeyboardButtonColor.POSITIVE)
-    keyboard.add(Text("И что-то?", {"cmd": "2"}), color=KeyboardButtonColor.SECONDARY)
+    keyboard.add(Text("Развлечение", {"cmd": "Развлечение"}), color=KeyboardButtonColor.SECONDARY)
     keyboard.row()
     keyboard.add(Location())
     keyboard.add(OpenLink("https://vk.com/club224358783", "Группа сообщества"))
@@ -115,72 +121,108 @@ async def weather_city(message:Message):
             await message.answer('Не удачно', keyboard=keyboard)
 
 
+@bot.on.private_message(text="Переводчик")
+@bot.on.message(payload={"cmd": "язык"})
+async def language(message: Message):
+  key = Keyboard(one_time=True)
+  key.add(Text("Русский",{"cmd":"ru"}),color=KeyboardButtonColor.POSITIVE)
+  key.add(Text("English", {"cmd" : "en"}), color=KeyboardButtonColor.NEGATIVE)
+  await message.answer("Выберите язык",keyboard=key)
 
-@bot.on.message(text="И что-то?")
-@bot.on.message(payload={"cmd": "пусто"})
+@bot.on.private_message(text=['ru'])
+@bot.on.message(payload={"cmd": "ru"})
+async def ru(message: Message):
+  ctx_storage.set("language", "ru")
+  await message.answer("Введите текст для перевода")
+  await bot.state_dispenser.set(message.peer_id, Translate.translator)
+
+@bot.on.private_message(text=['en'])
+@bot.on.message(payload={"cmd": "en"})
+async def en(message: Message):
+  ctx_storage.set("language", "en")
+  await message.answer("Введите текст для перевода")
+  await bot.state_dispenser.set(message.peer_id, Translate.translator)
+  
+@bot.on.message(state=Translate.translator)
+@bot.on.message(payload={"cmd": "еще"})
+async def translator_text(message: Message):
+  ctx_storage.set("translator",message.text)
+  translator = ctx_storage.get("translator") 
+  language = ctx_storage.get("language")
+  keyboard = Keyboard(one_time=True)
+  keyboard.add(Text("Назад", {"cmd": "я умею"}), color=KeyboardButtonColor.NEGATIVE)
+  keyboard.add(Text("Еще", {"cmd": "еще"}), color=KeyboardButtonColor.POSITIVE)
+  if language == "ru":
+    ans = gs.translate(translator, 'ru')
+    await message.answer(ans,keyboard=keyboard)
+    await bot.state_dispenser.delete(message.peer_id, Translate.translator)
+
+  elif language == "en":
+    ans = gs.translate(translator, 'en')
+    await message.answer(ans,keyboard=keyboard)
+    await bot.state_dispenser.delete(message.peer_id, Translate.translator)
+
+
+@bot.on.message(text="Развлечение")
+@bot.on.message(payload={"cmd": "Развлечение"})
 async def menu(message: Message):
     keyboard = Keyboard(one_time=True)
-    keyboard.add(Text("пока пусто", {"cmd": "пусто"}), color=KeyboardButtonColor.SECONDARY)
+    keyboard.add(Text("Камень,Ножницы,Бумага", {"cmd": "кнб"}), color=KeyboardButtonColor.SECONDARY)
+    keyboard.add(Text("Угадай число", {"cmd": "Угадай число"}), color=KeyboardButtonColor.POSITIVE)
     keyboard.add(Text("Назад", {"cmd": "меню"}), color=KeyboardButtonColor.NEGATIVE)
-    await message.answer(message="Выберай", keyboard=keyboard)
+    await message.answer(message="Выбирай", keyboard=keyboard)
 
 
+@bot.on.message(text="Камень,Ножницы,Бумага")
+@bot.on.message(payload={"cmd": "кнб"})
+async def game_handler(message: Message):
+    await message.answer("Выберите свой вариант: камень, ножницы или бумага")
+  
+@bot.on.message(text="камень")
+@bot.on.message(text="ножницы")
+@bot.on.message(text="бумага")
+async def play_handler(message: Message):
+  choices = ["камень", "ножницы", "бумага"]
+  bot_choice = random.choice(choices)
 
-@bot.on.private_message(text=['Переводчик'])
-async def note_handler(message: Message):
-    text = message.text
-    
+  user_choice = message.text.lower()
 
-@bot.on.message(state=Translate.translator) 
-async def translator_text(message: Message):
-    ctx_storage.set("translator",message.text)
-    translator = ctx_storage.get("translator") 
-    text = message.text
-    keyboard = Keyboard(one_time=True)
-    keyboard.add(Text("Назад", {"cmd": "я умею"}), color=KeyboardButtonColor.NEGATIVE)
-   
- 
-    if text[0].lower() in ru_letters:
-      translator = message
-      translator = Translator(from_lang="russian", to_lang='english')
- 
-      
-    elif text[0].lower() in en_letters:  
-      translator = Translator(from_lang="english", to_lang='russian')
-    
-      
-    else:
-      await message.answer("Я тебя не понял")
-      return
-      
-    translation = translator.translate(text) 
-    await message.answer (translation, keyboard=keyboard)
-     
+  if user_choice == bot_choice:
+      result = "Ничья!"
+  elif (user_choice == "камень" and bot_choice == "ножницы") or (user_choice == "ножницы" and bot_choice == "бумага") or (user_choice == "бумага" and bot_choice == "камень"):
+      result = "Вы победили!"
+  else:
+      result = "Вы проиграли!"
+
+  await message.answer(f"Вы выбрали {user_choice}, бот выбрал {bot_choice}. {result}")
 
 
-    # translate = Translator()
-    # lang = translate.detect(message.text)
-    # lang = lang.lang
-    # if lang == 'ru':
-    #   send = translate.translate(message.text)
-    #   await message.answer(message, '------\n'+ send.text +'\n------')
+@bot.on.message(text=['Угадай число'])
+@bot.on.message(payload={"cmd": "Угадай число"})
+async def guess_number(message: Message):
+  await message.answer("Угадай число от 1 до 100")
 
-    # else:
-    #   send = translate.translate(message.text, dest='ru')
-    #   await message.answer(message, '------\n'+ send.text +'\n------')
+  number_to_guess = random.randint(1, 100)
+
+  while True:
+      response = await bot.api_ctx.messages.get_by_conversation_message_id(
+          peer_id=message.peer_id,
+          conversation_message_ids=[message.conversation_message_id],
+      )
+      user_number = int(response.items[0].text)
+
+      if user_number < number_to_guess:
+          await message.answer("Загаданное число больше")
+      elif user_number > number_to_guess:
+          await message.answer("Загаданное число меньше")
+      else:
+          await message.answer("Поздравляю, вы угадали число!")
 
 
-@bot.on.private_message(text=['пока пусто'])
-@bot.on.message(payload={"cmd": "пусто"})
-async def pysto(message: Message):
-    await message.answer(message="пусто", keyboard=keyboard)
 
 @bot.on.private_message()
 async def noknow(message: Message):
     await message.answer(message='Я тебя не понимаю, воспользуйся командой "меню" ',)
-# @bot.on.chat_message()
-# async def messange_detect(messange: Message):
-#     await messange.answer(messange.text)
 
-
+  
 bot.run_forever()
